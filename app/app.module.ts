4a4a5c147acf043f177eb539b5d46ebde2c08062
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-09-29 17:26:11 +0530

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { GridModule,SelectionService, PageService, SortService, ToolbarService, EditService } from '@syncfusion/ej2-ng-grids';
+import { GridModule,SelectionService, PageService, SortService, FilterService, ToolbarService, EditService } from '@syncfusion/ej2-ng-grids';
 import { ButtonModule } from '@syncfusion/ej2-ng-buttons';
 import { ToolbarModule } from '@syncfusion/ej2-ng-navigations';
 import { AppComponent }  from './app.component';
@@ -9,6 +9,6 @@ import { AppComponent }  from './app.component';
   imports:      [ BrowserModule, GridModule, ButtonModule, ToolbarModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [PageService, SelectionService, SortService, ToolbarService, EditService]
+  providers: [PageService, SelectionService, SortService, ToolbarService, FilterService, EditService]
 })
 export class AppModule { }
